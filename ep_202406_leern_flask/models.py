6fb13b89bba8d37e83bf8578e691779e444f1672
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-06-20 15:27:27 +0500

    Добавил DocStrings в файл

--- ep_202406_leern_flask/models.py
+++ ep_202406_leern_flask/models.py
@@ -16,11 +16,34 @@ db = SQLAlchemy()
 
 
 class Artist(db.Model):
+    """Класс Артист - в данном классе находятся имена артистов и групп
+
+    Args:
+        db (SQLAlchemy): Базовый класс SQLAlchemy
+        
+    Атрибуты:
+        id - Идентификатор
+        name - ИМя артиста или группы
+    """
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100), nullable = False)
      
         
 class Album(db.Model):
+    
+    """Класс Альбом: В данном классе находятся назмания альбомов, которые выпустили
+    артисты или группы. Данный класс также пересекаетя с классом Артист
+
+    Args:
+        db (SQLAlchemy): Базовый класс SQLAlchemy
+        
+    Атрибуты:
+        id  - Идентификатор
+        title - Название альбома
+        year - Год выпуска альбома
+        artist_id -  внешний ключ (из таблицы Artist)
+    
+    """
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(100), nullable = False)
     year = db.Column(db.String(4), nullable = False)
@@ -29,10 +52,24 @@ class Album(db.Model):
     
 
 class Song(db.Model):
+    """
+    Класс Песни - в данном классе находятся наименования песен артистов.
+    Класс Songs также пересекается с классом Album
+
+    Args:
+        db (SQLAlchemy): Базовый класс SQLAlchemy
+        
+    Атрибуты:
+        id - Идентификатор
+        title - Наименование песни
+        lehgth - длинна песни
+        track_number - номер трека в альбоме
+        album_id - внешний ключ(из таблицы Album )
+    """
     # TODO КМК артиста надо добавить, иначе, чтобв посмотреть кто поет  - понадобиться каждый раз скакать по таблицам
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(100), nullable = False)
-    lenght = db.Column(db.String(4), nullable = False)
+    length = db.Column(db.String(4), nullable = False)
     track_number = db.Column(db.String(4), nullable = False)
     album_id = db.Column(db.Integer, db.ForeignKey('album.id'), nullable = False)
     album = db.relationship('Album', backref = db.backref('songs' , lazy = True))
