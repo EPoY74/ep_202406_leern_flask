28c013f9ddaae70185bd7b95e46a6dbbd964e15f
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-06-25 06:38:44 +0500

    Пишу функцию, котораяя ваводит все знеяения атрибутов у my_api_request().request. Зачем: Стало интересно, а можно ли это реализовать!

--- ep_202406_leern_flask/testing.py
+++ ep_202406_leern_flask/testing.py
@@ -1,13 +1,20 @@
 """
 Разбираюсь мо сравнением времени, так как не взлетело сразу.
+Тестовй файл, буду в нем ковырять и тыкать палкой
+
 Сайт урока: https://proglib.io/p/samouchitel-po-python-dlya-nachinayushchih-chast-23-osnovy-veb-razrabotki-na-flask-2023-06-27
 Вынес отдельно, так как  нужно было отдебвжить
 
 Returns:
-    Приветствие, в зависимости от времени  суток
+    Приветствие, в зависимости от времени  суток и не только
+    
+    
 """
 
 import datetime
+
+
+import requests as reqs
 
 
 def str_to_time_conv(now_time : str):
@@ -21,9 +28,6 @@ def str_to_time_conv(now_time : str):
         datetime : Формат датавремени для корректного сравнения
     """
     return datetime.datetime.strptime(now_time, "%H:%M:%S").time()
-
-
-print(type(datetime.datetime.now().hour))
 
 
 def greeting():
@@ -49,26 +53,53 @@ def greeting():
     else: times_greeting = "Фигня какая-то "
     return times_greeting
     
-if __name__=="__main__":
-    print(greeting())
-    
-    
-    
-    """_summary_
-    Все оказалось проще:
-    
-    now = datetime.datetime.now()
-    if now.hour >= 6 and now.hour < 12:
-        greeting = 'Доброе утро'
-    elif now.hour >= 12 and now.hour < 18:
-        greeting = 'Добрый день'
-    elif now.hour >= 18 and now.hour < 24:
-        greeting = 'Добрый вечер'
-    else:
-        greeting = 'Доброй ночи'
-    
-    
-    Так как hour возврящает просто int... :^(
-        
+
+def my_api_request():
+
+    """Работаю и изучаю реквесты.
+       Сайт урока: https://proglib.io/p/python-i-api-prevoshodnoe-kombo-dlya-avtomatizacii-raboty-s-publichnymi-dannymi-2021-02-26
     """
+    responce_request = reqs.get("https://randomuser.me/api/")
+    # responce_request = reqs.get("")
+    return  responce_request
     
+if __name__=="__main__":
+       # print(greeting())
+    print(my_api_request().text)
+    print(my_api_request().request.url)
+    
+
+    all_attributes = dir(my_api_request().request)
+    
+    print(type(all_attributes))
+    
+    for index_of_attribute, attribute in enumerate(all_attributes, start = 1):
+        print(f"{index_of_attribute}: {attribute}")
+    
+    for index, attribute in enumerate (all_attributes):
+        if attribute.find("_") == 0:
+            all_attributes[index] = " "
+    
+    for index_of_attribute, attribute in enumerate(all_attributes, start = 1):
+        print(f"{index_of_attribute}: {attribute}")
+    
+    print(20*"_")   
+    
+    clear_list = [attribute for attribute in all_attributes if len(attribute) != 1 and attribute != " "]
+        
+    print(20*"_")
+    
+    for index_of_attribute, attribute in enumerate(clear_list, start = 1):
+        print(f"{index_of_attribute}: {attribute}")
+        
+    print(20*"_")
+
+    # #Выведите их на экран
+    # for index_of_atribute, attribute in enumerate(all_attributes, statr = 1):
+    #     # print(attribute + " "+ my_api_request().str(attribute))
+    #     if attribute.index("_"):
+    #         continue
+        
+    #     value = str(getattr(my_api_request, attribute))
+    #     print(f"{index_of_atribute}: my_api_request().{attribute}: {value}")
+        
